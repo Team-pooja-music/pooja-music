83f8bcac646bee18e6d7a1bceb32ac89cb57a4ba
Team-pooja-music <monubhai708019@gmail.com>
2024-12-18 21:32:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Ksdofficial8/BIT_MUSIC",
+    "https://github.com/team-pooja-music/pooja-music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
